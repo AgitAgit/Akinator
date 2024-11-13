3f861ce43c2d5c44deddedaac64ee3da9a924165
Amit Kubani <amitkubani@gmail.com>
2024-11-13 11:41:55 +0200

    commit

--- Client/Communication_Center/userData.js
+++ Client/Communication_Center/userData.js
@@ -1,7 +1,8 @@
-const base_url = 'https://iitc-b-backend-server-akinator-project-w.onrender.com'
+const base_url = 'https://iitc-b-backend-server-akinator-project-w.onrender.com/api/users';
+const local_base_url = 'http://localhost:3000/api/users';
 
 export const registerUser = async function(firstName, userName, password, email){
-    const reply = await axios.post(`${base_url}/api/users/users`,{
+    const reply = await axios.post(`${base_url}/users`,{
         fname: firstName,
         name: userName,
         password: password,
