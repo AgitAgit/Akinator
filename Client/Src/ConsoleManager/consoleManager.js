5134ad9338c8b2b5418627a8f550e559e13beec5
Amit Kubani <amitkubani@gmail.com>
2024-11-14 14:14:01 +0200

    commit

--- Client/Src/ConsoleManager/consoleManager.js
+++ Client/Src/ConsoleManager/consoleManager.js
@@ -2,7 +2,7 @@
 //of the updateScreen effect.
 
 //Maybe break it into separate functions. On to write 
-//sentences that will be pretty quick(50 miliseconds delay 
+//sentences that will be pretty quick(50 milliseconds delay 
 //for each character?) and a slower one for a small
 //delay at the end of each word.
 
@@ -10,7 +10,7 @@
 //Maybe I should just attach spans and <br> elements for each sentence
 //In the update screen function.
 
-const screenText = document.querySelector("#screen-text");
+const screen = document.querySelector(".deviceScreen");
 
 const testText = "This is a test for the update screen.\nTell all the truth but tell it slant\nsuccess in circuit lies\nToo bright for our infirm delight\nthe truths superb surprise\nAs lightning to the children eased\nwith explanation kind\nThe truth must dazzle gradually\nor every man be blind";
 
@@ -26,7 +26,6 @@ document.querySelectorAll('button').forEach(button => button.addEventListener('c
 //could add a click sound to all buttons with query selector all
 
 export const initScreen = function(){
-    screenText.textContent = "test2 test2 test2";
     const intervalId = setInterval(screenFlicker, 1500);
     updateScreen(testText);
 }
@@ -34,11 +33,16 @@ export const initScreen = function(){
 export const updateScreen = function(text, counter = 0){
     if(counter === 0){
         clearScreen();
+        const screenText = document.createElement("pre");
+        screenText.classList.add("screen-text");
+        screen.appendChild(screenText);
     }
+
     if(counter === text.length){
         return;
     }
-    screenText.textContent += text.charAt(counter);
+
+    document.querySelector(".screen-text").textContent += text.charAt(counter);
     setTimeout(() => updateScreen(text, counter + 1), 100);
 }
 
@@ -48,15 +52,15 @@ async function screenFlicker(){
 }
 
 function screenOn(){
-    screenText.classList.remove("hidden");
+    document.querySelector(".screen-text")?.classList.remove("hidden");
 }
 
 function screenOff(){
-    screenText.classList.add("hidden");
+    document.querySelector(".screen-text")?.classList.add("hidden");
 }
 
 function clearScreen(){
-    screenText.textContent = "";
+    document.querySelector(".deviceScreen").innerHTML = "";
 }
 
 function playRandomClick(){}
--- Client/Src/main.js
+++ Client/Src/main.js
@@ -3,3 +3,11 @@ import { prompt, restartSession, restartGame } from "./Communication_Center/gptD
 import { initScreen } from "./ConsoleManager/consoleManager.js";
 
 initScreen();
+
+//main flow:
+//Show the menu in the console -> start as guest -> initGame
+
+
+//menu options: start as guest, login(opens login/signup page)
+
+
