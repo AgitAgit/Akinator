5d3b7eb282d006f476792300f894a02dc30e9564
Amit Kubani <amitkubani@gmail.com>
2024-11-15 19:01:03 +0200

    commit

--- Client/Src/main.js
+++ Client/Src/main.js
@@ -6,6 +6,9 @@ const yesButton = document.querySelector("#yes");
 const noButton = document.querySelector("#no");
 const maybeButton = document.querySelector("#maybe");
 
+document.querySelectorAll(".choice-button").forEach(button => {
+    button.addEventListener('click', (event) => handleChoiceButtonClick(event));
+})
 
 const defaultUser = {
     fName: "guest",
@@ -14,29 +17,29 @@ const defaultUser = {
     email: "guest"
 }
 
-// const test = await registerUser("guest","guest","guest","guest");
-// console.log("test:", test);
+let mode = 'pre-game';
 
-document.querySelectorAll(".choice-button").forEach(button => {
-    button.addEventListener('click', (event) => handleChoiceButtonClick(event));
-})
-
-let mode = 'game';
+async function startGame(user = defaultUser){
+    console.log("Trying to login user...");
+    const data = await loginUser(user.email, user.password);
+    mode = 'game';
+    console.log("mode:", mode, "data:", data);
+}
 
 function handleChoiceButtonClick(event){
     const text = event.target.textContent;
     console.log("handle choice button click says:", text);
-    // if(mode === 'game'){
-    //     prompt()
-    // }
+    if(mode === 'game'){
+        prompt(token, text);
+    }
 }
 
 initScreen();
+
+startGame();
 
 //main flow:
 //Show the menu in the console -> start as guest -> initGame
 
 
 //menu options: start as guest, login(opens login/signup page)
-
-
